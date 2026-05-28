10ef2b9f25b82f81781420578b1a51cf0d58d06a
munyuaDeveloper <pmunyua@safaricom.co.ke>
2026-05-28 20:14:40 +0300

    Add stats and new role management

--- api/index.ts
+++ api/index.ts
@@ -19,7 +19,7 @@ const connectDB = async () => {
     }
 
     await mongoose.connect(DB);
-    console.log('MongoDB connected successfully');
+    console.log('MongoDB connected successfully!');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
